373ac8920092d17e9774fcfdb5a7ce67ebc93614
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-04-12 15:28:05 -0300

    Files Upload

--- JavaScript/2-Advancing-in-js/1-Arrays/script.js
+++ JavaScript/2-Advancing-in-js/1-Arrays/script.js
@@ -1,5 +1,4 @@
 let numbers = [2, 1, 4, 3, 5]
-numbers.push(6)
 
 console.log(numbers.sort())
 //console.log(numbers.length) //In js length is one atribute and don't need '()'
@@ -7,9 +6,28 @@ console.log(numbers.sort())
 // for (let c = 0;c <= numbers.length;c++) {
 //     console.log(numbers[c])
 // }
-let c = 0
-for (c in numbers) {
+for (let c in numbers) {
     console.log(numbers[c])
 }
+
+//Array Methods
+
 console.log(numbers.indexOf(4)) //Position of value in array
-console.log(numbers.indexOf(0)) //Value not found
+console.log(numbers.indexOf(0)) //Value not found; -1
+
+// Convert string to array
+
+const txt = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle'
+let array = txt.split(',')
+console.log(array[1])
+
+// Convert array to string
+
+array = ['Rocket','Flash','Bella','Slugger']
+console.log(array.toString())
+
+numbers.push(6) //Insert 6 (value) to last of array
+console.log(numbers)
+numbers.pop() //Remove last array item
+console.log(numbers)
+
