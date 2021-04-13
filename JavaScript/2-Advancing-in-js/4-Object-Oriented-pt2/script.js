efbdd61332c508e3afb2537c07e7c9d792587637
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-04-13 11:42:05 -0300

    Files Upload

--- JavaScript/2-Advancing-in-js/4-Object-Oriented-pt2/script.js
+++ JavaScript/2-Advancing-in-js/4-Object-Oriented-pt2/script.js
@@ -1,3 +1,21 @@
 // Object() Constructor
 
-let person = new Object()
+let person = new Object() //Stores empity object in variable, method used to generic objects
+
+//Adding properties and methods
+
+person.name = 'Kevin'
+person.greeting = function() {console.log(`Hello, I\'m Kevin... Again`)}
+
+//Testing Object
+
+person.greeting()
+
+//Using Create() method
+
+let person2 = Object.create(person)  
+
+person2.name = 'Invasor'
+person2.greeting = function() {console.log(`Now your code is my!`)}
+
+person2.greeting()
