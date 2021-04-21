71092123700f09cdf358c511efb3456e7f2b6524
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-04-21 19:59:23 -0300

    Files Upload

---
+++ JavaScript/zSites whith Js/7-add-persons/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('#button')
+const display = document.querySelector('#display')
+
+button.addEventListener('click', istancier)
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age =  age
+    }
+    bio() {
+        return `${this.name} has ${this.age} years old`
+    }
+    this() {
+        return console.log(this)
+    }
+}
+function istancier() {
+    const name = document.getElementById('name').value
+    const age = Number(document.getElementById('age').value)
+    const person = new Person(name, age)
+    display.innerHTML += person.bio()+'<br>'
+    person.this()
+}
