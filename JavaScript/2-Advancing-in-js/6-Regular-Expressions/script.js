54c65633071082ecb9e2489215665a2c71ac7746
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-04-26 17:42:46 -0300

    Files Update

--- JavaScript/2-Advancing-in-js/6-Regular-Expressions/script.js
+++ JavaScript/2-Advancing-in-js/6-Regular-Expressions/script.js
@@ -11,7 +11,7 @@ console.log()
 
 //the re object
 
-const text = '12345Stop Losing Your Time12345'
+let text = '12345SToP LOSiNG YoUr TiMe12345'
 re = /(stop losing |stop |lose )(your time)/i // () makes string divided in grups, | == 'or'
 let found = re.exec(text)
 
@@ -23,9 +23,23 @@ if (found){ // if found != null, null == not found set
     console.log(found[0]) // Full set
     console.log(found[1]) // Grup 1
     console.log(found[2]) // Grup 2
-
     console.log()
 
     console.log(found.input) //return the input
     console.log(found.index) //return the index of set appears in string
 } else {console.error('Value not found')}
+
+// using string methods with regex
+
+console.log(found[0].toLowerCase().replace('time', 'money')) // Replacing words
+
+console.log()
+
+text = 'js js js jS Js #js#'
+re = /js/gi // 'g' is the global flag, her search for all ocorrences in string
+
+console.log(text.match(re))
+
+let s = 0 
+for (c in text.match(re)) {s += 1} //Sum
+console.log(s)
