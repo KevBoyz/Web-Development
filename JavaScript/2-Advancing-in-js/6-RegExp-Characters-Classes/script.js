56ef43bfd4bda0df2fe18c11fe85894d6ba5348e
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-04-27 17:05:26 -0300

    Files Upload

--- JavaScript/2-Advancing-in-js/6-RegExp-Characters-Classes/script.js
+++ JavaScript/2-Advancing-in-js/6-RegExp-Characters-Classes/script.js
@@ -5,7 +5,7 @@
 // The big letters do invert process
 // ( \x != \X ) 
 
-let text = '~!?;xA1bx;´¨&`-2bCc3 D_-=*/-+^{}}[]()%$ #@dE5eFf4Gg⤏☣☠☺∾∜☳㊮㊑'
+let text = '~!?;xA1bx ;x´¨&`-2bCc3 D_-=*/-+^{}}[]()%$ #@dE5eFf4Gg⤏☣☠☺∾∜☳㊮㊑'
 
 console.log('d', text.match(/\d/g).sort()) //| \d == 0-9 (lating numbers)
 console.log('D', text.toLowerCase().match(/\D/g).sort())
@@ -20,4 +20,8 @@ console.log('b', text.match(/\b/g)) // b == backspace ( )
 
 // locating the 'A1bx' 
 
-console.log(text.match(/\w\d\w\w/)[0])
+console.log(text.match(/\w\d\w{2}\b/)[0]) // \w{2} == \w\w
+
+// locating ~!?;x
+
+console.log(text.match(/\S{5}/)[0])
---
+++ JavaScript/2-Advancing-in-js/6-RegExp-Quantifiers/script.js
@@ -0,0 +1,15 @@
+console.log('teeeeeeeestiing'.match(/te*sti*ng/)[0]) // Matches the preceding item "x" 0 or more times
+console.log('pPyytthhoonN'.match(/p+y+t+h+o+n+/i)[0]) // Matches the preceding item "x" 1 or more times
+
+console.log('TwO tTtiMMes'.match(/x?two?/i)[0]) // Matches the preceding item "x" 0 or 1 time
+
+console.log('0XxXxXxX0'.match(/x{7}/i)[0]) // x{n} n = number of x's in string
+console.log('Github, Githuub, Githuuub'.match(/Githu{2,}b/i)[0]) // x{n,} == minimum ocorrences of x
+console.log('Fourrrr, fiveeeee'.match(/(four{5,9})|(five{4,5})/i)[0]) //x{mx,mn} == max, minimum
+
+// Example: find all word that ends with our
+
+let text = 'He asked his neighbour a favour.'
+const regex = /\w+ou?r\b/gi
+
+console.table(text.match(regex))
